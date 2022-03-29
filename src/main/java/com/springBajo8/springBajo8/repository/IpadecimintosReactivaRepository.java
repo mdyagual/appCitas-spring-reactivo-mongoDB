ebfd60a4c4458fb4f42a2d9a05addec5a803b8c1
Jeaniel Osorno <jeanielosorno@gmail.com>
2022-03-29 17:01:23 -0500

    Interfaces creadas para los dominios de padecimiento y tratamientos

---
+++ src/main/java/com/springBajo8/springBajo8/repository/IpadecimintosReactivaRepository.java
@@ -0,0 +1,13 @@
+package com.springBajo8.springBajo8.repository;
+
+import com.springBajo8.springBajo8.domain.padecimientosDTOReactiva;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+import reactor.core.publisher.Flux;
+
+public interface IpadecimintosReactivaRepository extends ReactiveMongoRepository<padecimientosDTOReactiva, String>{
+
+    Flux<padecimientosDTOReactiva> findByIdPadecimientos(String idPadecimiento);
+    
+}
---
+++ src/main/java/com/springBajo8/springBajo8/repository/ItratamientosReactivaRepository.java
@@ -0,0 +1,13 @@
+package com.springBajo8.springBajo8.repository;
+
+import com.springBajo8.springBajo8.domain.tratamientosDTOReactiva;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+import reactor.core.publisher.Flux;
+
+public interface ItratamientosReactivaRepository extends ReactiveMongoRepository<tratamientosDTOReactiva, String>{
+
+    Flux<tratamientosDTOReactiva> findByIdTratamientos(String idTratamiento);
+    
+}
